d134edb7176788f65f04ca73c3915d418f05ab48
DomenicP <domenicpaul@me.com>
2014-04-02 11:47:46 -0400

    Split qual matches and elim matches

--- Rakefile
+++ Rakefile
@@ -1,8 +1,14 @@
 require "rake/testtask"
+require 'benchmark'
 
 Rake::TestTask.new do |t|
   t.libs << 'src'
   t.libs << 'test'
+end
+
+desc "Benchmark the app"
+task :benchmark do
+  puts Benchmark.measure { ruby "src/mar_scouting.rb" }
 end
 
 desc "Run Tests"
--- src/mar_scouting.rb
+++ src/mar_scouting.rb
@@ -2,23 +2,31 @@ require_relative 'tba'
 require 'CSV'
 require 'pry'
 
+EVENT = "2014mrcmp"
+
 # Get the list of teams attending MAR DCMP
-@teams = TBA.get_event_teams "2014mrcmp"
+puts "Retrieving team list for #{EVENT}"
+@teams = TBA.get_event_teams EVENT
 @teams.sort_by! { |t| t['team_number'] }
 
 # Retrive match data for the teams
-@data = @teams.map { |t| TBA.get_team t['team_number'] }
+puts "Getting data for teams..."
+@data = @teams.map do |t|
+  puts "Team #{t['team_number']}: #{t['nickname']}"
+  TBA.get_team t['team_number']
+end
 
-CSV.open('MAR_Scouting.csv', 'w') do |csv|
+CSV.open("data/#{EVENT}.csv", 'w') do |csv|
   # Header row
-  csv << ['Team #', 'Name', 'Wins', 'Losses', 'Ties', 'High Score', 'Average Score', 'Awards']
+  csv << ['Team #', 'Name', 'Wins', 'Losses', 'Ties', 'Win/Loss Ratio', 'High Score (Q)', 'Average Score (Q)', 'High Score (E)', 'Average Score (E)', 'Awards']
   @data.each do |t|
     row = []
-    row << t['team_number']    # Team Number
-    row << t['nickname']       # Team Name
+    row << t['team_number']
+    row << t['nickname']
     
     wins = losses = ties = 0
-    scores = []
+    qual_scores = []
+    elim_scores = []
     awards = []
     t['events'].each do |e|
       e['matches'].each do |m|
@@ -34,7 +42,12 @@ CSV.open('MAR_Scouting.csv', 'w') do |csv|
           team_score = m['alliances']['blue']['score']
           opponent_score = m['alliances']['red']['score']
         end
-        scores << team_score
+
+        if m['comp_level'] == 'qm'
+          qual_scores << team_score
+        else
+          elim_scores << team_score
+        end
 
         # Determine if match was a win, loss, or tie
         if team_score > opponent_score
@@ -53,8 +66,12 @@ CSV.open('MAR_Scouting.csv', 'w') do |csv|
     row << wins
     row << losses
     row << ties
-    row << scores.max
-    row << ((scores.inject(0) { |sum, score| sum += score }) / scores.count)
+    row << (wins.to_f / losses.to_f).round(2)
+    row << qual_scores.max
+    row << ((qual_scores.inject(0) { |sum, score| sum += score }) / qual_scores.count)
+    row << elim_scores.max
+    row << ((elim_scores.inject(0) { |sum, score| sum += score }) / elim_scores.count)
+
     row << awards.join('; ')
     
     csv << row
